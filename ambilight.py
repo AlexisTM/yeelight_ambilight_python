74ef483d5e769857d3e10dcb8ccf71a8692a8fbf
AlexisTM <alexis.paques@gmail.com>
2020-05-09 19:59:24 +0200

    First draft.

---
+++ ambilight.py
@@ -0,0 +1,132 @@
+#!/bin/env python3
+import time
+import yeelight
+from PIL import ImageGrab, Image
+
+import numpy as np
+from colour import sRGB_to_XYZ, XYZ_to_xy, xy_to_CCT
+
+DO_MAIN = True
+DO_MAIN_MOON = False
+DO_AMBIENT = True
+
+DELAY = 0.05
+MAX_MAIN_LUMINOSITY = 15
+
+class MultiClient:
+    def __init__(self, ip):
+        self.clients = [
+            yeelight.Bulb(
+                ip,
+                effect="smooth",
+                duration=100,
+                auto_on=False,
+                model=yeelight.BulbType.WhiteTempMood,
+            ),
+            yeelight.Bulb(
+                ip,
+                effect="smooth",
+                duration=100,
+                auto_on=False,
+                model=yeelight.BulbType.WhiteTempMood,
+            ),
+            yeelight.Bulb(
+                ip,
+                effect="smooth",
+                duration=100,
+                auto_on=False,
+                model=yeelight.BulbType.WhiteTempMood,
+            ),
+        ]
+        self.index = 0
+
+    def get(self):
+        self.index += 1
+        return self.clients[self.index % 3]
+
+    def send_command(self, method, params):
+        self.get().send_command(method, params).get("result", [])
+
+def RGB_dist(rgb1, rgb2):
+    return abs(rgb1[0] - rgb2[0]) + abs(rgb1[1] - rgb2[1]) + abs(rgb1[2] - rgb2[2])
+
+
+def RGB_to_luminosity(rgb):
+    return int(
+        (rgb[0] * 299 / 1000 + rgb[1] * 114 / 1000 + rgb[2] * 587 / 1000) * 100 / 255
+    )
+
+
+def RGB_to_CCT(rgb):
+    RGB = np.array(rgb)
+    XYZ = sRGB_to_XYZ(RGB / 255)
+    xy = XYZ_to_xy(XYZ)
+    CCT = xy_to_CCT(xy, "hernandez1999")
+    return CCT
+
+
+def main():
+    last_CCT = 0
+    last_L = 0
+    last_L_Ambient = 0
+    last_RGB = (0, 0, 0)
+    while True:
+        image = ImageGrab.grab()
+        image.crop(image.getbbox())
+        image = image.resize((100, 100), resample=Image.NEAREST)
+        # R, G, B = image.getpixel((0,0))
+        converted_image = image.convert("P", palette=Image.ADAPTIVE, colors=16)
+        # converted_image.show()
+        colors = sorted(converted_image.getcolors(16))
+        # print(colors))
+        palette = converted_image.getpalette()
+        dominant_color = colors[-1]
+        rgb = palette[dominant_color[1] * 3 : dominant_color[1] * 3 + 3]
+        luminosity = RGB_to_luminosity(rgb)
+        cct = RGB_to_CCT(rgb)
+
+        if DO_MAIN:
+            if DO_MAIN_MOON and abs(luminosity - last_L) > 10:
+                lights.send_command("set_scene", ["nightlight", luminosity])
+            elif abs(luminosity - last_L) > 25 or abs(last_CCT - cct) > 300:
+                effective_luminosity = luminosity * MAX_MAIN_LUMINOSITY / 100
+                print(cct)
+                lights.send_command("set_scene", ["ct", cct, effective_luminosity])
+            last_L = luminosity
+            last_CCT = cct
+
+        if DO_AMBIENT:
+            if luminosity < 15:
+                if last_L_Ambient != 0:
+                    lights.send_command(
+                        "bg_set_scene",
+                        [
+                            "color",
+                            yeelight.utils.rgb_to_yeelight(rgb[0], rgb[1], rgb[2]),
+                            luminosity,
+                        ],
+                    )
+                    last_L_Ambient = 0
+            else:
+                if RGB_dist(last_RGB, rgb) > 25:
+                    lights.send_command(
+                        "bg_set_scene",
+                        [
+                            "color",
+                            yeelight.utils.rgb_to_yeelight(rgb[0], rgb[1], rgb[2]),
+                            100,
+                        ],
+                    )
+                    last_RGB = rgb
+
+        time.sleep(DELAY)
+
+
+lights = MultiClient("192.168.178.20")
+lights.get().turn_on(yeelight.LightType.Main)
+lights.get().turn_on(yeelight.LightType.Ambient)
+
+try:
+    main()
+except:
+    lights.get().turn_off()
